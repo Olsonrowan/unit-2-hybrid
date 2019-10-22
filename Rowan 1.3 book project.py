c636371743a22d0a3f90d5a804bf9ed3a02be55b
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2019-10-21 18:45:38 -0600

    Add files via upload

---
+++ Rowan 1.3 book project.py
@@ -0,0 +1,102 @@
+firstName = 'Rowan'
+lastName = 'Olson'
+print(firstName, lastName)
+
+# question 1
+print('true')
+
+# question 2
+print('true')
+
+# question 3
+print('false')
+
+# question 4
+print('true')
+
+# question 5
+print('true')
+
+# question 6
+print("true")
+
+# question 7
+print('true')
+
+# question 8
+print('false')
+
+# question 9
+print('true')
+
+# question 10
+print('true')
+
+print("multiple choice: ")
+# question 1
+print("C. fee setting")
+# question 2
+print('A. F= 9/5(C)+32')
+# question 3
+print('D. Specification')
+# question 4
+print('C. 2spam')
+# question 5
+print('B. Statements')
+# question 6
+print('B. Expressions')
+# question 7
+print('B. Program')
+# question 8
+print('D. a counted loop')
+# question 9
+print('C. Simultaneous')
+# question 10
+print('D. input')
+
+print("Discussion")
+# question 1
+print('Analyze the problem: ')
+print('find the needs, wants, and use' )
+print('Determine Specifications:')
+print('reiterate what the program will accomplish, explain what the program is doing to relate to the needs and wants')
+print('Create a design: ')
+print('have an image of what design will need to meet the specification')
+print('Implement the design: ')
+print('go forward and converting your designs into computer language')
+print('Test/Debug: ')
+print('finding and major AND minor glitches as well as anything that could possibly break the program and finding a solution for them.')
+print('Maintain the program: ')
+print('this step speaks for itself, keep the program and language up to date. ')
+# question 2
+print('this is chaotic function')
+x = eval(input("enter a number between 0 and 1")) #this is input
+for i in range(10): #this is loop
+    x = 3.9 * x * (1 - x) #this is assignment
+    print(x) # this is output
+# question 3
+print('Definite loop: ')
+print('loops that execute a known number of times, the for statement is a definite loop that iterates the sequence of values')
+print("Counted loop: ")
+print('loops uses range to produce a sized sequence of numbers')
+print('For loops: ')
+print('designed specifically for repeating a portion of the program')
+# question 4
+print('A.)')
+for i in range(5):
+    print(i * i)
+print('B.)')
+for d in [3,1,4,1,5]:
+    print(d, end=" ")
+print('C.)')
+for i in range(4):
+    print("hello")
+print('D.)')
+for i in range(5):
+    print(i, 2**i)
+# question 5
+print("you are able to test what your program must do in a natural language before inputing it into the programming language")
+# question 6
+print('sep is a soft space feature and can be made into any other character, interger or string,')
+# question 7
+print('i tested it and there was no more variables so it went back to the body therfore skipping the "hello"')
